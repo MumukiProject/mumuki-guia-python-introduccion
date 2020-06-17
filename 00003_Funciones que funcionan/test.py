975ae6119ce2ef4da8969ab8a79bbd2dd4925655
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-06-17 18:45:39 +0000

    Mumuki Export on 2020-06-17 18:45:39 +0000

--- 00003_Funciones que funcionan/test.py
+++ 00003_Funciones que funcionan/test.py
@@ -1,10 +1,10 @@
 #...content...#
 #...extra...#
-
+user_first_name=#...user_first_name...#
 
 class Test(unittest.TestCase):
-      def test_prueba_con_varias_entradas(self):
-      for entrada in range(-1, 2):
-          salida_esperada=entrada / 2
-          salida=leer_mente(entrada)
-          self.assertEqual(salida_esperada,salida,f" en otras palabras: la salida de leer_mente({entrada}), fué {salida} , pero <b>esperábamos que fuera {salida_esperada}. Probá nuevamente, #...user_first_name...#</b>")
+  def test_prueba_con_varias_entradas(self):
+    for entrada in range(-1, 2):
+      salida_esperada=entrada / 2
+      salida=leer_mente(entrada)
+      self.assertEqual(salida_esperada,salida,f" en otras palabras: la salida de leer_mente({entrada}), fué {salida} , pero <b>esperábamos que fuera {salida_esperada}. Probá nuevamente, {user_first_name}</b>")
