fb696a01d33ff1ba6062b81215933d0312abb1f8
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-06-17 15:56:07 +0000

    Mumuki Export on 2020-06-17 15:56:07 +0000

--- 00003_Funciones que funcionan/test.py
+++ 00003_Funciones que funcionan/test.py
@@ -1,13 +1,11 @@
 import datetime
 from extra import ejercicio_mente
 
-print=ejercicio_mente.pausa_print
-
 class Test(unittest.TestCase):
 
   def test_leer_mente_div2(self):
     antes=ejercicio_mente.ahora()
-    print("impreso a las {}".format(antes))
+    ejercicio_mente.pausa_print("impreso a las {}".format(antes))
     despues=ejercicio_mente.ahora()
     print("intervalo: {} segundos".format(despues-antes))
     for entrada in range(-1,2):
