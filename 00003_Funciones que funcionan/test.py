71b7ca6a80878c9d9f217f87f32405f15d792b7e
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-06-17 17:48:21 +0000

    Mumuki Export on 2020-06-17 17:48:21 +0000

--- 00003_Funciones que funcionan/test.py
+++ 00003_Funciones que funcionan/test.py
@@ -1,15 +1,4 @@
-/*...extra...*/
-/*...content...*/
-import datetime
-
-
 class Test(unittest.TestCase):
 
-  def test_probemos_leer_mente(self):
-    ejercicio_mente.a_veeer()
-    antes=ejercicio_mente.ahora()
-    ejercicio_mente.pausa_print("impreso a las {}".format(antes))
-    despues=ejercicio_mente.ahora()
-    print("intervalo: {} segundos".format(despues-antes))
-    for entrada in range(-1,2):
-      self.assertEqual(entrada/2,leer_mente(entrada))
+  def test_description_example(self):
+    self.assertTrue(True)
