aa8cd24c2a08e6de10499b4ffef4fc34c831edb1
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-06-18 02:14:22 +0000

    Mumuki Export on 2020-06-18 02:14:22 +0000

--- 00003_Funciones que funcionan/test.py
+++ 00003_Funciones que funcionan/test.py
@@ -7,5 +7,5 @@ class Test(unittest.TestCase):
     for entrada in range(-1, 2):
       salida_esperada=entrada / 2
       salida=leer_mente(entrada)
-      self.assertEqual(salida_esperada,salida,f" en otras palabras: la salida de leer_mente({entrada}), fué {salida} , pero <b>esperábamos que fuera {salida_esperada}.</b>")
+      self.assertEqual(salida_esperada,salida,f" en otras palabras: la salida de leer_mente({entrada}), fué {salida} , pero esperábamos que fuera {salida_esperada}")
     self.assertFalse(True,str(globals()))
